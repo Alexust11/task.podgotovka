f93030b754eda69d67e327c16361a52e49b920be
Alexust11 <110272504+Alexust11@users.noreply.github.com>
2023-02-18 22:50:55 +0700

    задача 1,2

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.ArrayList.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //1. Дан List <String> names. Удалите первую букву из каждого имени и верните отсортированный список
+//        List<String> names=List.of("Алекс", "Федор", "Игорь","Павел");
+//
+//        names.stream().map(e->e.substring(1)).sorted().forEach(System.out::println);
+//
+//    }
+        //****************************************************************************
+        // 2. Напишите программу, которая переворачивает массив
+//        String[] array = {"a1", "a2", "a3"};
+//        Stream<String> streamFromArrays = Arrays.stream(array);
+//        streamFromArrays.sorted(Collections.reverseOrder()).forEach(System.out::println);
+        //****************************************************************************
+    }
+}
